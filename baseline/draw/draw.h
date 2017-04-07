17dfb4c03d497fc9d9dfb2e67c8329fbbe3185e0
Wesley Smith <wesley.hoke@gmail.com>
2017-04-06 22:33:15 -0700

    format

--- baseline/draw/draw.h
+++ baseline/draw/draw.h
@@ -4,9 +4,9 @@
 #include <array>
 
 namespace baseline {
-namespace draw {
-void ClearWindowWithDepth(std::array<uint32_t, 2> window_size, uint32_t color);
-}
+    namespace draw {
+        void ClearWindowWithDepth(std::array<uint32_t, 2> window_size, uint32_t color);
+    }
 }
 
 #endif
